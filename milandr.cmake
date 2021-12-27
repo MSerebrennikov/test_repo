0dd5f5f81524d54348cf4411f418d3d22fa07c17
MSerebrennikov <serebrennikovmu@sinara-group.com>
2021-12-27 10:18:07 +0500

    комит для теста отката с изменениями

---
+++ milandr.cmake
@@ -0,0 +1,21 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy CACHE INTERNAL "")
+set(CMAKE_OBJDUMP arm-none-eabi-objdump CACHE INTERNAL "")
+
+# tells CMake not to try to link executables during its interal checks
+# things are not going to link properly without a linker script
+#set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+
